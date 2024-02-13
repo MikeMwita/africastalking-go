68ce1d17107a1d2016457468783a41b449c4deaf
MikeMwita <mwitamike8@gmail.com>
2024-02-13 14:17:04 +0300

    Add Sms entity

--- domain/entities/sms_sender.go
+++ domain/entities/sms_sender.go
@@ -1,12 +1,18 @@
 package entities
 
 type SMSSender struct {
-	Key        string   `json:"key"`        // the unique identifier of the sender
-	APIKey     string   `json:"apiKey"`     // the API key from the AfricasTalking API
-	APIUser    string   `json:"apiUser"`    // the username from the AfricasTalking API
-	Recipients []string `json:"recipients"` // the list of phone numbers to send the message to
-	Message    string   `json:"message"`    // the message content
-	Sender     string   `json:"sender"`     // the sender ID
+	Key                  string   `json:"key"`        // the unique identifier of the sender
+	APIKey               string   `json:"apiKey"`     // the API key from the AfricasTalking API
+	APIUser              string   `json:"apiUser"`    // the username from the AfricasTalking API
+	Recipients           []string `json:"recipients"` // the list of phone numbers to send the message to
+	Message              string   `json:"message"`    // the message content
+	Sender               string   `json:"sender"`     // the sender ID
+	Title                any
+	BulkSMSMode          int
+	RetryDurationInHours int
+	LinkId               string
+	Keyword              string
+	Enqueue              int
 }
 
 type SMSMessageData struct {
